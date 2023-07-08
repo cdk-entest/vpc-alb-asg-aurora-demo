a40cb81bd3256c82e0b4adfbe745bfdcfa97dff8
entest <entest>
2023-07-08 05:36:15 +0000

    test

--- lib/script/user-data-2.sh
+++ lib/script/user-data-2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # kill -9 $(lsof -t -i:8080)
+export SECRET_ID=AuroraStackIcaDatabaseSecre-VKUdDUkkkWIc
 export REGION=ap-southeast-1
-wget https://github.com/cdk-entest/vpc-alb-asg-aurora-demo/archive/refs/heads/main.zip
-unzip main.zip 
-cd vpc-alb-asg-aurora-demo-main/
+wget https://github.com/cdk-entest/vpc-alb-asg-aurora-demo/archive/refs/heads/master.zip 
+unzip master.zip 
+cd vpc-alb-asg-aurora-demo-master/
 python3 -m ensurepip --upgrade
 python3 -m pip install -r requirements.txt
 cd web-app
