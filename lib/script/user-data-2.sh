5a8a910de8cde8381867d91040dd8f752edfa039
entest <entest>
2023-07-08 05:39:59 +0000

    update user data

--- lib/script/user-data-2.sh
+++ lib/script/user-data-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # kill -9 $(lsof -t -i:8080)
-export SECRET_ID=AuroraStackIcaDatabaseSecre-VKUdDUkkkWIc
+export SECRET_ID=aurora-secrete-name
 export REGION=ap-southeast-1
 wget https://github.com/cdk-entest/vpc-alb-asg-aurora-demo/archive/refs/heads/master.zip 
 unzip master.zip 
