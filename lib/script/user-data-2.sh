0c7bf00abfbbc36d72642817211ebcabdca93fc1
entest <entest>
2023-07-09 22:29:10 +0000

    update

--- lib/script/user-data-2.sh
+++ lib/script/user-data-2.sh
@@ -2,6 +2,7 @@
 # kill -9 $(lsof -t -i:8080)
 export SECRET_ID=aurora-secrete-name
 export REGION=ap-southeast-1
+wget -O ~/.vimrc https://github.com/cdk-entest/basic-vim/blob/main/.vimrc
 wget https://github.com/cdk-entest/vpc-alb-asg-aurora-demo/archive/refs/heads/master.zip 
 unzip master.zip 
 cd vpc-alb-asg-aurora-demo-master/
